57868220ffb61394a6d3625c0db4ba3ffb7b4012
Finlay Williams <finlay@realtimeagency.com>
2025-08-13 15:27:28 +0000

    if we treat every round of the tournament as a "column" each column has

--- src/app/app/tournaments/page.tsx
+++ src/app/app/tournaments/page.tsx
@@ -522,9 +522,9 @@ export default function TournamentsPage() {
                             </ScrollArea>
                         </div>
                         <div className="overflow-auto p-4">
-                            <div className="relative flex h-full w-full items-start justify-center">
+                            <div className="relative flex h-full w-full items-center justify-center">
                                 {bracketRounds.length > 0 ? (
-                                    <div className="inline-flex items-start gap-x-12">
+                                    <div className="inline-flex items-center gap-x-12">
                                         {bracketRounds.map((round, roundIndex) => (
                                             <div key={roundIndex} className="flex flex-col justify-around h-full w-48 space-y-4">
                                                 <h4 className="text-center font-bold text-lg mb-4">{round.title}</h4>
@@ -553,3 +553,5 @@ export default function TournamentsPage() {
     </div>
   );
 }
+
+    
